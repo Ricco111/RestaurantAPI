c9e3b7b8291af6f35687010d268c652cdfbad08e
Ricco111 <e.szugzda@gmail.com>
2022-06-24 18:26:17 +0200

    added: Account Controller RegisterUserDto AccountServices

---
+++ Services/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Services
+{
+    public interface IAccountService
+    {
+        public void RegisterUser(RegisterUserDto dto);
+    }
+}
